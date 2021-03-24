32cceee623d252bdfaa375759b8dd989b5ef34d9
Serena Graham <serenaelizabeth@hotmail.co.uk>
2021-03-24 10:30:28 +0000

    auth0

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -3,37 +3,48 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import CreateEvent from "../CreateEvent";
 import Profile from "../Profile";
 import GroupFeed from "../GroupFeed";
+import LoginButton from "../LoginButton";
+import LogoutButton from "../LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Profile</Link>
-            </li>
-            <li>
-              <Link to="/CreateEvent">Create Event</Link>
-            </li>
-            <li>
-              <Link to="/GroupFeed">Group Feed</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/CreateEvent">
-            <CreateEvent />
-          </Route>
-          <Route path="/GroupFeed">
-            <GroupFeed />
-          </Route>
-          <Route path="/">
-            <Profile />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      {!isAuthenticated && <LoginButton></LoginButton>}
+      {isAuthenticated && <LogoutButton></LogoutButton>}
+
+      {isAuthenticated && (
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Profile</Link>
+                </li>
+                <li>
+                  <Link to="/CreateEvent">Create Event</Link>
+                </li>
+                <li>
+                  <Link to="/GroupFeed">Group Feed</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route path="/CreateEvent">
+                <CreateEvent />
+              </Route>
+              <Route path="/GroupFeed">
+                <GroupFeed />
+              </Route>
+              <Route path="/">
+                <Profile />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      )}
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Components/App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-xd17tm81.eu.auth0.com"
+      clientId="vxsvZ9xZ9QlMwkaKrD2eJYqEG6Gmjbg9"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
