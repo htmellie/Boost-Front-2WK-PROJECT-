50b4285834c9460faac0fa258dc92be39e525d98
lucaxue <lxue02@gmail.com>
2021-03-25 20:19:13 +0000

    navbar responsive

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,14 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CreateEvent from "../CreateEvent";
-import Profile from "../Profile";
-import GroupFeed from "../GroupFeed";
-import { useAuth0 } from "@auth0/auth0-react";
-import LoginPage from "../LoginPage";
-import NavBar from "../NavBar";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CreateEvent from '../CreateEvent';
+import Profile from '../Profile';
+import GroupFeed from '../GroupFeed';
+// import { useAuth0 } from "@auth0/auth0-react";
+import LoginPage from '../LoginPage';
+import NavBar from '../NavBar';
 
 function App() {
-  const { isAuthenticated } = useAuth0();
+  // const { isAuthenticated } = useAuth0();
+  const isAuthenticated = true;
   return (
     <Router>
       <Switch>
@@ -18,7 +19,9 @@ function App() {
         <Route path="/GroupFeed">
           <GroupFeed />
         </Route>
-        <Route path="/">{isAuthenticated ? <Profile /> : <LoginPage />}</Route>
+        <Route path="/">
+          {isAuthenticated ? <CreateEvent /> : <LoginPage />}
+        </Route>
       </Switch>
 
       {isAuthenticated && <NavBar />}
--- src/Components/CreateEvent/index.js
+++ src/Components/CreateEvent/index.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import { Center, Heading } from '@chakra-ui/layout';
+
 function CreateEvent() {
-  return <h2>Create Event</h2>;
+  return (
+    <Center>
+      <Heading>Create Event</Heading>
+    </Center>
+  );
 }
 
 export default CreateEvent;
