12848a3d6ba3adf690a24aa60d9cd8bbc0464dc3
Ellie <ellieredmayne@hotmail.com>
2021-03-25 16:01:41 +0000

    Calendar Picker

--- src/Components/CreateEvent/index.js
+++ src/Components/CreateEvent/index.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import GenericButton from '../GenericButton';
-import { Box, Grid, GridItem, HStack, Heading } from '@chakra-ui/react';
-
+import React from "react";
+import GenericButton from "../GenericButton";
+import { Box, Grid, GridItem, HStack, Heading } from "@chakra-ui/react";
+import DateAndTimePickers from "../TimeAndDatePicker";
 
 function CreateEvent() {
-
-    function checkButtonClicks(num){
-      console.log(`you clicked ${num}`);
-    }
+  function checkButtonClicks(num) {
+    console.log(`you clicked ${num}`);
+  }
 
   return (
     <Box>
-      <Heading >Create Event</Heading>
+      <Heading>Create Event</Heading>
 
-      <GenericButton text={'What is your event?'} handleClick={()=>checkButtonClicks(1)}/>
-      <GenericButton text={'Where is your?'} handleClick={()=>checkButtonClicks(2)}/>
-      <GenericButton text={'What do you want to do?'} handleClick={()=>checkButtonClicks(3)}/>
-      <GenericButton text={'Submit'} handleClick={()=>checkButtonClicks(4)}/>
+      <GenericButton
+        text={"What is your event?"}
+        handleClick={() => checkButtonClicks(1)}
+      />
+      <DateAndTimePickers />
+      <GenericButton
+        text={"Where is your?"}
+        handleClick={() => checkButtonClicks(2)}
+      />
+      <GenericButton
+        text={"What do you want to do?"}
+        handleClick={() => checkButtonClicks(3)}
+      />
+      <GenericButton text={"Submit"} handleClick={() => checkButtonClicks(4)} />
     </Box>
-  )
+  );
 }
 
 export default CreateEvent;
