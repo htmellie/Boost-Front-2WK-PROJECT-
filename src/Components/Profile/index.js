863da74aeaa003f2433efa5d104322feb9fdb1d8
Serena Graham <serenaelizabeth@hotmail.co.uk>
2021-03-25 10:34:23 +0000

    centring

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -1,32 +1,40 @@
 import LogoutButton from "../LogoutButton";
 import { Box, Heading, Grid, GridItem, Center } from "@chakra-ui/react";
+import GenericButton from "../GenericButton";
 
 function Profile({ user, isAuthenticated, isLoading }) {
   return (
-    <Box>
-      {isAuthenticated && <LogoutButton />}
-      <Grid
-        bg="#FACD60"
-        width="75%"
-        height="75%"
-        margin="auto"
-        mt="5"
-        alignContent="center"
-      >
-        {isAuthenticated && (
-          <div>
-            <GridItem>Welcome, {user.name}!</GridItem>
-            <GridItem>
-              <img src={user.picture} alt={user.name} />
-            </GridItem>
-            <GridItem>{user.email}</GridItem>
-          </div>
-        )}
-      </Grid>
-    </Box>
+    isAuthenticated && (
+      <Box>
+        <LogoutButton />
+        <Grid
+          placeItems="center"
+          bg="#FACD60"
+          width="75%"
+          height="75%"
+          margin="auto"
+          mt="5"
+        >
+          <GridItem>Welcome, {user.name}!</GridItem>
+          <GridItem>
+            <img src={user.picture} alt={user.name} />
+          </GridItem>
+          <GridItem>{user.email}</GridItem>
+          <GridItem>Group</GridItem>
+          <GridItem>Next Session:</GridItem>
+          <GenericButton
+            text="Create a group"
+            handleClick={() => console.log("click")}
+          ></GenericButton>
+          <GenericButton
+            text="Join a group"
+            handleClick={() => console.log("click")}
+          ></GenericButton>
+        </Grid>
+      </Box>
+    )
   );
 }
-
 /*ON THE PROFILE PAGE
 Picture in the middle
 Logout button
