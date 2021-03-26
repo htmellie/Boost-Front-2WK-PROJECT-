905084b1b2455253563732ab16096e1c5098f38f
cavedave2 <sflanag5@gmail.com>
2021-03-26 16:00:40 +0000

    login button changes

--- src/Components/LoginButton/index.js
+++ src/Components/LoginButton/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "@chakra-ui/react";
+import { Button, Grid } from "@chakra-ui/react";
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
   return (
+    <Grid placeItems="center" width="75%" height="75%" margin="auto" mt="50%">
     <Button
       bg="#1AC0C6"
-      size="md"
+      size="lg"
       textColor="#fff"
       onClick={() => loginWithRedirect()}
     >
-      Log In
+      Log In To Boot
     </Button>
+    </Grid>
   );
 };
 
