c2e0d496e1f7892b4bc42cc515aaf842aa334eb2
Serena Graham <serenaelizabeth@hotmail.co.uk>
2021-03-26 16:22:50 +0000

    event time

--- src/Components/DateAndTimePicker/index.js
+++ src/Components/DateAndTimePicker/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
   textField: {
     marginLeft: theme.spacing(1),
@@ -18,7 +18,7 @@ export default function DateAndTimePickers({ dispatch }) {
   const classes = useStyles();
 
   function setDateAndTime(e) {
-    dispatch({ type: 'SET_DATE_AND_TIME', payload: e.target.value });
+    dispatch({ type: "SET_DATE_AND_TIME", payload: e.target.value });
   }
 
   return (
@@ -26,7 +26,7 @@ export default function DateAndTimePickers({ dispatch }) {
       <TextField
         onChange={setDateAndTime}
         id="datetime-local"
-        label="Next appointment"
+        label="Event Time"
         type="datetime-local"
         defaultValue="2017-05-24T10:30"
         className={classes.textField}
