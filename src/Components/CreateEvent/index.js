9385298902029ade3cb1ad6d0bc477ec2ce765e7
Ellie <ellieredmayne@hotmail.com>
2021-03-26 13:44:24 +0000

    dropdown renamed

--- src/Components/CreateEvent/index.js
+++ src/Components/CreateEvent/index.js
@@ -1,43 +1,38 @@
-import React, {useState, useEffect,useReducer} from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import GenericButton from "../GenericButton";
 import { Box, Grid, GridItem, HStack, Heading, VStack } from "@chakra-ui/react";
 import DateAndTimePickers from "../TimeAndDatePicker";
-import WhatExercise from "../WhatExercise";
-import EventMap from '../Map/index';
-
+import ExerciseDropdown from "../ExerciseDropdown";
+import EventMap from "../Map/index";
 
 function CreateEvent() {
   function checkButtonClicks(num) {
     console.log(`you clicked ${num}`);
   }
-  
+
   //need text box which updates value
   //need to get lat and long form Eventmap...maybe send down a setstate
   //need to get date and time from calander
   //does the whole thing need to be in a form?
 
-const[eventWhen,setEventWhen]=useState();
+  const [eventWhen, setEventWhen] = useState();
 
+  const [eventWhere, setEventWhere] = useState();
+  const [eventWhat, setEventWhat] = useState();
 
-const[eventWhere,setEventWhere]=useState();
-const[eventWhat,setEventWhat]=useState();
-  
-console.log(eventWhen);
-console.log(eventWhat);
-console.log(eventWhere);
+  console.log(eventWhen);
+  console.log(eventWhat);
+  console.log(eventWhere);
 
   return (
     <Box>
       <VStack>
         <Heading>Create Event</Heading>
 
-
         <GenericButton
           text={"When is your event?"}
           handleClick={() => checkButtonClicks(1)}
         />
-
-
 
         <DateAndTimePickers setEventWhen={setEventWhen} />
         <GenericButton
@@ -45,13 +40,13 @@ console.log(eventWhere);
           handleClick={() => checkButtonClicks(2)}
         />
 
-<EventMap />
+        <EventMap />
 
-        <GenericButton 
+        <GenericButton
           text={"What do you want to do?"}
           handleClick={() => checkButtonClicks(3)}
         />
-        <WhatExercise setEventWhat={setEventWhat}/>
+        <ExerciseDropdown setEventWhat={setEventWhat} />
 
         <GenericButton
           text={"Submit"}
