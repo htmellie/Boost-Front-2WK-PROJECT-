1fb4ae693f97c3cad07dfca2851c2728b904c35c
Ellie <ellieredmayne@hotmail.com>
2021-03-26 10:32:20 +0000

    map done

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -1,37 +1,71 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, {
+  useState,
+  useRef,
+  useMemo,
+  useCallback,
+  useEffect,
+} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 // import 'leaflet/dist/leaflet.css';
+// predetermined long and lat for map
+function EventMap() {
+  const center = {
+    lat: 51.505,
+    lng: -0.09,
+  };
+  // makes the marker draggable to choose a location for event
+  function DraggableMarker() {
+    const [draggable, setDraggable] = useState(false);
+    const [position, setPosition] = useState(center);
+    const markerRef = useRef(null);
+    const eventHandlers = useMemo(
+      () => ({
+        dragend() {
+          //finds the final location of marker
+          const marker = markerRef.current;
+          if (marker != null) {
+            setPosition(marker.getLatLng());
+          }
+        },
+      }),
+      []
+    );
+    const toggleDraggable = useCallback(() => {
+      setDraggable((d) => !d);
+    }, []);
+    console.log(position);
+    return (
+      <Marker //the blue marker on page
+        draggable={draggable}
+        eventHandlers={eventHandlers}
+        position={position}
+        ref={markerRef}
+      >
+        <Popup minWidth={90}>
+          <span onClick={toggleDraggable}>
+            {draggable
+              ? "Marker is draggable"
+              : "Click here to make marker draggable"}
+          </span>
+        </Popup>
+      </Marker> //pop up that tells user the maker is draggable
+    );
+  }
 
-function EventMap(){
-
-function notifyClick(){
-  console.log('map got clicked');
+  return (
+    //the map rendered
+    <>
+      <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <DraggableMarker />
+      </MapContainer>
+    </>
+  );
 }
-
-
-//hard coded variables
-let long=52.4862;
-let lat=-1.8904;
-
-    return(
-        <div id='mapid'>
-    <MapContainer center={[long, lat]} zoom={13} scrollWheelZoom={false} >
-  <TileLayer
-    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[long, lat]}>
-    <Popup>
-      A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-  </Marker>
-</MapContainer>
-</div>
-    )
-}
-    
 // #mapid { height: 180px; }
 
 export default EventMap;
-
-
