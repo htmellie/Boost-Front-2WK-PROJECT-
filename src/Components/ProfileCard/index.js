194fc6d54247231c2ba187c6bb29095c3705127a
Ellie <ellieredmayne@hotmail.com>
2021-04-01 11:46:14 +0100

    colours added

--- src/Components/ProfileCard/index.js
+++ src/Components/ProfileCard/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Grid, GridItem, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  HStack,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 function ProfileCard({
   firstName,
   surname,
@@ -13,12 +21,12 @@ function ProfileCard({
     <Grid
       boxShadow="lg"
       placeItems="center"
-      padding="30px 50px"
-      borderRadius={[null, "10px", "10px"]}
-      bg="white"
+      padding="30px 30px"
+      borderRadius={["10px", "10px", "10px", "10px"]}
+      bg="#1ac0c6"
       templateColumns={[null, null, "4fr 5fr"]}
       transition="0.2s"
-      width={["100%", "70%", "max-content"]}
+      width={["95%", "70%", "max-content"]}
       margin="10%"
     >
       <Image
@@ -32,51 +40,57 @@ function ProfileCard({
         bg="white"
         margin="10%"
       />
-      <GridItem>
-        <GridItem padding="10px 0">
-          <HStack>
-            <Heading
-              size={"xl"}
-              fontSize={["2rem", "2rem", "3rem"]}
-              color="gray.800"
-            >
-              {`${firstName} ${surname}`}
-            </Heading>
-          </HStack>
-          <Text
-            marginTop="-5px"
-            color="gray.400"
-            size="xs"
-            fontSize={["1em", "1em", "1.5em"]}
-          >
-            {"@" + username}
-          </Text>
-        </GridItem>
-        <GridItem padding="10px 0" fontSize={["1em", "1em", "1.2em"]}>
-          <Heading size="xs" color="gray.300">
-            {isAdmin ? "ADMIN" : "PART OF"}
-          </Heading>
-          <Text size="sm">{group}</Text>
-        </GridItem>
-        <GridItem margin="10px 0">
-          <HStack>
+      <Box
+        bg="white"
+        padding="5%"
+        borderRadius={["10px", "10px", "10px", "10px"]}
+      >
+        <GridItem>
+          <GridItem padding="10px 0">
+            <HStack>
+              <Heading
+                size={"xl"}
+                fontSize={["2rem", "2rem", "3rem"]}
+                color="gray.800"
+              >
+                {`${firstName} ${surname}`}
+              </Heading>
+            </HStack>
             <Text
-              className="material-icons"
-              color="gray.500"
-              fontSize={["1.5em", "1.5em", "2.5em"]}
+              marginTop="-5px"
+              color="gray.400"
+              size="xs"
+              fontSize={["1em", "1em", "1.5em"]}
             >
-              schedule
+              {"@" + username}
             </Text>
-            <Heading
-              size="xl"
-              color="gray.500"
-              fontSize={["1.5em", "1.5em", "2.5em"]}
-            >
-              {hours + " HOURS"}
+          </GridItem>
+          <GridItem padding="10px 0" fontSize={["1em", "1em", "1.2em"]}>
+            <Heading size="xs" color="gray.300">
+              {isAdmin ? "ADMIN" : "PART OF"}
             </Heading>
-          </HStack>
-        </GridItem>
-      </GridItem>
+            <Text size="sm">{group}</Text>
+          </GridItem>
+          <GridItem margin="10px 0">
+            <HStack>
+              <Text
+                className="material-icons"
+                color="gray.500"
+                fontSize={["1.5em", "1.5em", "2.5em"]}
+              >
+                schedule
+              </Text>
+              <Heading
+                size="xl"
+                color="gray.500"
+                fontSize={["1.5em", "1.5em", "2.5em"]}
+              >
+                {hours + " HOURS"}
+              </Heading>
+            </HStack>
+          </GridItem>
+        </GridItem>{" "}
+      </Box>
     </Grid>
   );
 }
