6598afcf4dc8eedf3c517acbd5ebdfff5b205990
lucaxue <lxue02@gmail.com>
2021-04-01 12:33:10 +0100

    border around map picker

--- src/Components/LocationMapPicker/index.js
+++ src/Components/LocationMapPicker/index.js
@@ -5,7 +5,7 @@ import {
   FormControl,
   FormHelperText,
   FormLabel,
-  GridItem,
+  Grid,
 } from '@chakra-ui/react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
@@ -25,18 +25,26 @@ function LocationMapPicker({ dispatch }) {
   return (
     <FormControl padding="5px 0">
       <FormLabel>Pick your location</FormLabel>
-      <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <DraggableMarker position={position} setPosition={setPosition} />
-      </MapContainer>
-      <FormHelperText>
-        Press the map to find your location, drag the pin to choose your
-        location.
-      </FormHelperText>
-      <Button onClick={setLocation}>Set Location</Button>
+      <Grid
+        border="1px"
+        borderColor="gray.200"
+        borderRadius="7px"
+        padding="10px"
+        placeItems="center"
+      >
+        <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <DraggableMarker position={position} setPosition={setPosition} />
+        </MapContainer>
+        <FormHelperText>
+          Press the map to find your location, drag the pin to choose your
+          location.
+        </FormHelperText>
+        <Button onClick={setLocation}>Set Location</Button>
+      </Grid>
     </FormControl>
   );
 }
